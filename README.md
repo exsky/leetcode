d0d97865b362bff32afc6d732c74e0b44ff8b967
Alex Yang <alexyang@qnap.com>
2019-04-02 18:17:05 +0800

    init commit, solve to-lower-case

---
+++ README.md
@@ -0,0 +1,4 @@
+# Leetcode
+
+## Easy
+1. [To Lower Case](https://leetcode.com/problems/to-lower-case/)
---
+++ easy/to_lower_case/main.go
@@ -0,0 +1,9 @@
+import (
+    "strings"
+)
+
+func toLowerCase(str string) string {
+    var out string
+    out = strings.ToLower(str)
+    return out
+}
